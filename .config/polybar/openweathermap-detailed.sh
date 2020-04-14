002f3bc7f4124fef0dd3f586fe4ccb7557b08d03
Cyfraka <cyfraka@gmail.com>
2020-04-14 18:07:00 +0200

    update Polybar with Weather

--- .config/polybar/openweathermap-detailed.sh
+++ .config/polybar/openweathermap-detailed.sh
@@ -44,7 +44,7 @@ get_icon() {
 KEY="71f5d9774715507c7751f6a33c259772"
 CITY="Krakow"
 UNITS="metric"
-SYMBOL="°"
+SYMBOL="°C"
 
 API="https://api.openweathermap.org/data/2.5"
 
@@ -68,10 +68,10 @@ else
 fi
 
 if [ -n "$weather" ]; then
-    weather_desc=$(echo "$weather" | jq -r ".weather[0].description")
+   #weather_desc=$(echo "$weather" | jq -r ".weather[0].description")
     weather_temp=$(echo "$weather" | jq ".main.temp" | cut -d "." -f 1)
     weather_icon=$(echo "$weather" | jq -r ".weather[0].icon")
 
-    echo "$(get_icon "$weather_icon")" "$weather_desc" "$weather_temp$SYMBOL"
+    echo "$(get_icon "$weather_icon")""$weather_desc" "$weather_temp$SYMBOL"
 fi
 
